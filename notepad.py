d059a6595b3f2053ccbe336dc6e9629796f26309
harismkr <mak.09.harr@gmail.com>
2024-06-03 18:40:17 +0300

    Add files via upload

---
+++ notepad.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+pyautogui.press("winleft")
+time.sleep(0.5)
+pyautogui.write("note")
+time.sleep(0.5)
+pyautogui.press("enter")
+time.sleep(0.5)
+pyautogui.write("hahahaha")
+time.sleep(0.5)
+pyautogui.hotkey("alt", "f4")
+time.sleep(0.5)
+pyautogui.press("tab")
+time.sleep(0.5)
+pyautogui.press("enter")
